65d5cd52f024890fd0408f17cb27d8e321e19e6a
Joris van Vugt <jorisvanvugt@live.nl>
2015-02-20 19:24:25 +0000

    Add imports for RenderingHints

--- src/com/jorisvanvugt/lolbadge/LoLBadge.java
+++ src/com/jorisvanvugt/lolbadge/LoLBadge.java
@@ -3,6 +3,7 @@ package com.jorisvanvugt.lolbadge;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
