3fb69541ee01f0107f586eb15e9257d78c575f10
Joris van Vugt <jorisvanvugt@live.nl>
2015-02-20 23:10:24 +0000

    Finalized badge design

--- src/com/jorisvanvugt/lolbadge/LoLBadge.java
+++ src/com/jorisvanvugt/lolbadge/LoLBadge.java
@@ -17,8 +17,8 @@ public class LoLBadge {
 	private static final int BADGE_HEIGHT = 732;
 	private static HashMap<String, BufferedImage> rankLogos = new HashMap<String, BufferedImage>();
 	private static BufferedImage LOL_LOGO;
-	private static final int RANK_WIDTH = 192;
-	private static final int RANK_HEIGHT = 192;
+	private static final int RANK_WIDTH = 256;
+	private static final int RANK_HEIGHT = 256;
 	private static final int LOL_WIDTH = 606;
 	private static final int LOL_HEIGHT = 230;
 	private static final int LOL_PADDING = 50;
@@ -31,7 +31,7 @@ public class LoLBadge {
 	public LoLBadge(Participant participant){
 		if(!imagesLoaded){
 			try {
-				LOL_LOGO = ImageIO.read(new File("res\\lol.png"));
+				LOL_LOGO = ImageIO.read(new File("res\\lolcolour.png"));
 				
 				rankLogos.put("Unranked", ImageIO.read(new File("res\\UNRANKED.png")));
 				rankLogos.put("Bronze", ImageIO.read(new File("res\\BRONZE.png")));
@@ -40,7 +40,7 @@ public class LoLBadge {
 				rankLogos.put("Platinum", ImageIO.read(new File("res\\PLATINUM.png")));
 				rankLogos.put("Diamond", ImageIO.read(new File("res\\DIAMOND.png")));
 				rankLogos.put("Master", ImageIO.read(new File("res\\MASTER.png")));
-				rankLogos.put("Challenger", ImageIO.read(new File("res\\Challenger.png")));
+				rankLogos.put("Challenger", ImageIO.read(new File("res\\CHALLENGER.png")));
 				
 				imagesLoaded = true;
 			} catch (IOException e) {
@@ -79,19 +79,20 @@ public class LoLBadge {
 		drawCentered(participant.getSummonerName(), 0, BADGE_HEIGHT / 2, g2d);
 		
 		g2d.setFont(new Font(FONT, Font.PLAIN, 72));
-		drawCentered(participant.getTeamName(), 0, (int) (BADGE_HEIGHT * 0.7), g2d);
+		drawCentered(participant.getTeamName(), 0, (int) (BADGE_HEIGHT * 0.63), g2d);
 		
-		g2d.setFont(new Font(FONT, Font.PLAIN, 48));
-		drawCentered(participant.getRank(), 0, (int) (BADGE_HEIGHT * 0.9), g2d);
+		g2d.setFont(new Font(FONT, Font.PLAIN, 72));
+		int rankX = drawCentered(participant.getRank(), RANK_WIDTH / 2, (int) (BADGE_HEIGHT * 0.87), g2d);
 		
-		g2d.drawImage(rankLogos.get(participant.getRank().split(" ")[0]), BADGE_WIDTH / 2 - 300, BADGE_HEIGHT - RANK_HEIGHT, RANK_WIDTH, RANK_HEIGHT, null);
+		g2d.drawImage(rankLogos.get(participant.getRank().split(" ")[0]), rankX - RANK_WIDTH, BADGE_HEIGHT - RANK_HEIGHT, RANK_WIDTH, RANK_HEIGHT, null);
 		g2d.drawImage(LOL_LOGO, BADGE_WIDTH / 2 - LOL_WIDTH / 2, LOL_PADDING, LOL_WIDTH, LOL_HEIGHT, null);
 		return image;
 	}
 	
-	private void drawCentered(String s, int xPos, int yPos, Graphics2D g2d){
+	private int drawCentered(String s, int xPos, int yPos, Graphics2D g2d){
 		int stringLen = (int) g2d.getFontMetrics().getStringBounds(s, g2d).getWidth();
 	    int start = BADGE_WIDTH / 2 - stringLen / 2;
 	    g2d.drawString(s, start + xPos, yPos);
+	    return start + xPos;
 	}
 }
